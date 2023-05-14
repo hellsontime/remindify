c10fa51047373cf86d71668a0a9187309934cec0
hellsontime <roma67177@gmail.com>
2023-05-14 16:48:28 +0200

    simple implementation for NotFoundComponent.jsx

---
+++ src/components/NotFoundComponent.jsx
@@ -0,0 +1,10 @@
+function NotFoundComponent() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you&#39;re looking for could not be found.</p>
+    </div>
+  );
+}
+
+export default NotFoundComponent;
